9cccafef87e9511e3b229e58a5b32e9c59d6c7c7
Swati-Kumari-Das <swatikumaridas1111@gmail.com>
2025-07-04 18:34:13 +0530

    new repo

---
+++ ai-interview-scheduler-ai-voice-agent/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Heloo g</h2>
+      <Button>Hello g!</Button>
+    </div>
+  );
+}
